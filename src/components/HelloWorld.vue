2f89a3a00a18eff203c7b2367a740b92a9ab9eb4
Sérgio Glórias <sergio@serginho.dev>
2022-06-18 03:50:00 +0100

    nova tentativa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
 <script setup>
-import axios from "axios";
+import axios from 'axios'
 </script>
 
 <template>
+
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>{{ ip }}</h3>
   </div>
+
 </template>
 
 <script>
@@ -14,29 +16,25 @@ export default {
   data() {
     return {
       msg: "Your IP",
-      ip: "Unknown",
+      ip: "Unknown"
     };
   },
   methods: {
     getIP() {
-      return axios
-        .get("/cdn-cgi/trace")
-        .then((res) => {
-          let ip = res.data.match(
-            /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
-          );
-          ip = ip ? ip[0] : "Unknown";
-          return ip;
-        })
-        .catch(() => {
-          return "Unknown";
-        });
-    },
+      const req = new XMLHttpRequest();
+      req.open('GET', "/cdn-cgi/trace", false);
+      req.send(null);
+      // is paintext, but works
+      let ip = req.responseText.match(/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/);
+      ip = ip ? ip[0] : "Unknown";
+      return ip;
+    }
   },
   mounted() {
     this.ip = this.getIP();
-  },
-};
+  }
+}
+
 </script>
 
 <style scoped>
@@ -44,6 +42,7 @@ h1 {
   font-weight: 500;
   font-size: 2.6rem;
   top: -10px;
+
 }
 
 h3 {
@@ -56,6 +55,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
